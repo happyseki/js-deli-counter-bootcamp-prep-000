695f0181279bf5320b9826b0d6d28c397173bbbb
Elaina <elainaseki@gmail.com>
2018-11-29 20:54:24 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+   katzDeliLine;
+  return(`Welcome,${name}.You are number ${katzDeliLine.length} in line.`);
+}
+takeANumber(katzDeliLine, 'Ada');
+takeANumber(katzDeliLine, 'Grace');
+takeANumber(katzDeliLine, 'Kent');
+
