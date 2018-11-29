56b88745c45110a1237dfb1e9e5860267dbcbb48
Elaina <elainaseki@gmail.com>
2018-11-29 20:54:50 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var katzDeliLine = [];
+function takeANumber(katzDeliLine, name){
+  
+  return `Welcome,${name}.You are number 1 in line.`;
+  katzDeliLine.push(name);
+  return katzDeliLine;
+}
+
+function nowServing(katzDeliLine){
+  for(i=0;i<katzDeliLine.length;i++){
+    
+  }
+  return console.log("There is nobody waiting to be served!");
+  
+}
