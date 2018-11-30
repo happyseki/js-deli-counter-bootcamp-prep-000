f5de15b46c17514ecd363daa6b6f1fd29a797d4a
Elaina <elainaseki@gmail.com>
2018-11-30 05:36:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 var katzDeliLine = [];
+<<<<<<< HEAD
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
  console.log(`Welcome,${name}.You are number ${katzDeliLine.length} in line.`);
    return katzDeliLine;
+=======
+ katzDeliLine.push(name);
+ katzDeliLine;
+function takeANumber(katzDeliLine, name){
+ 
+  return(`Welcome,${name}.You are number ${katzDeliLine.length} in line.`);
+>>>>>>> 50e7314eba529379142f6b83e2137329af83e406
 }
 takeANumber(katzDeliLine, 'Ada');
 takeANumber(katzDeliLine, 'Grace');
 takeANumber(katzDeliLine, 'Kent');
 
+<<<<<<< HEAD
 function nowServing(katzDeliLine){
   if(katzDeliLine.length>0){
   console.log(`We are serving ${katzDeliLine.shift()}`);
@@ -18,4 +27,8 @@ function nowServing(katzDeliLine){
 }
 
  
+=======
+
+
+>>>>>>> 50e7314eba529379142f6b83e2137329af83e406
 
