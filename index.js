59342495b030e61e990295dfeef31c19570ba3d5
Elaina <elainaseki@gmail.com>
2018-12-01 02:45:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-
 var katzDeliLine = [];
- function takeANumber(katzDeliLine, name){
+function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
-  return  `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+return`Welcome,${name}.You are number ${katzDeliLine.length} in line.`;
 }
+<<<<<<< HEAD
  function nowServing(katzDeliLine){
   if (!katzDeliLine.length){return "There is nobody waiting to be served!";
     } else {   return `Currently serving ${katzDeliLine.shift()}.`; }
@@ -19,4 +19,32 @@ var katzDeliLine = [];
     return output;
    }
  }
+=======
+ takeANumber(katzDeli, 'Ada');
+ takeANumber(katzDeli, 'Grace');
+ takeANumber(katzDeli, 'Kent');
+ 
+function nowServing(katzDeliLine){
 
+if(katzDeliLine.length>0){
+   return`Currently serving ${ katzDeliLine.shift()}`;
+    }
+  else{
+    return `There is nobody waiting to be served!`;
+  }
+}
+>>>>>>> 30c93fb91f804f20caece4872ae7a9a36a47d1bf
+
+function currentLine(katzDeliLine){
+   if(katzDeliLine.length===0){
+    return `The line is currently:empty.`;}
+   else {
+  var line=`The line is currently:`;
+  for(var i=0;i<katzDeliLine.length;i++){
+   line += `${(i+1)}.${katzDeliLine[i]}`;
+     if(i<katzDeliLine.length-1){
+    line +=`,`; }
+     return line;
+    }
+  }
+}
